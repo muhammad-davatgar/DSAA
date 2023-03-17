61892b6bd6cf5a0e2fa4ff26d5196472c32dbd5b
muhammad-davatgar <md2002.110@gmail.com>
2023-03-18 00:08:16 +0330

    removed Rc

--- src/dynamic/fib.rs
+++ src/dynamic/fib.rs
@@ -1,6 +1,6 @@
-use std::{cell::RefCell, collections::HashMap, rc::Rc};
+use std::{cell::RefCell, collections::HashMap};
 
-pub fn dynamic_fibonacci(n: u128, memo: Rc<RefCell<HashMap<u128, u128>>>) -> u128 {
+pub fn dynamic_fibonacci(n: u128, memo: &RefCell<HashMap<u128, u128>>) -> u128 {
     if n <= 2 {
         return 1;
     }
@@ -9,7 +9,7 @@ pub fn dynamic_fibonacci(n: u128, memo: Rc<RefCell<HashMap<u128, u128>>>) -> u12
         return *v;
     }
 
-    let res = dynamic_fibonacci(n - 1, memo.clone()) + dynamic_fibonacci(n - 2, memo.clone());
+    let res = dynamic_fibonacci(n - 1, &memo) + dynamic_fibonacci(n - 2, &memo);
     memo.borrow_mut().insert(n, res);
     return res;
 }
@@ -20,13 +20,9 @@ mod test {
 
     #[test]
     fn dynamic_fibonacci_works() {
-        let memo = Rc::new(RefCell::new(HashMap::new()));
-        assert_eq!(1, dynamic_fibonacci(1, memo.clone()));
-        assert_eq!(21, dynamic_fibonacci(8, memo.clone()));
-        assert_eq!(34, dynamic_fibonacci(9, memo.clone()));
-        assert_eq!(89, dynamic_fibonacci(11, memo.clone()));
-        assert_eq!(144, dynamic_fibonacci(12, memo.clone()));
-        assert_eq!(12586269025, dynamic_fibonacci(50, memo.clone()),);
+        let memo = RefCell::new(HashMap::new());
+
+        assert_eq!(12586269025, dynamic_fibonacci(50, &memo));
     }
 }
 
